59b45246f60c04b84bd662a1e3ce9c3e9d6717f1
Christian Castillo <castillochristian3@gmail.com>
2022-04-10 08:48:48 -0400

    changed h1 and added button funcitonality

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,16 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
+    pokemonButton.addEventListener('click', getrandomPokemon);
 
     
 })
+//NODE Getters
+const pokemonContainer = document.querySelector("#pokemon-container");
+const pokemonButton = document.querySelector("#get-pokemon");
 
+
+//Helper Functions
 function randomId(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min)
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 function getrandomPokemon() {
     fetch(`https://pokeapi.co/api/v2/pokemon/${randomId(1,809)}`)
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(pokemon => renderPokemon(pokemon))
+}
+
+function renderPokemon(pokemon) {
+    const pokemonDiv = document.createElement("div");
+    pokemonDiv.innerHTML = `
+        ${pokemon.name}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png">
+    `
+    pokemonContainer.appendChild(pokemonDiv);
+     
 }

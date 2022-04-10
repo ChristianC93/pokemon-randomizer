e31f8596ff8509554325af2d0b5c66eb7c9753c7
Christian Castillo <castillochristian3@gmail.com>
2022-04-10 10:46:18 -0400

    added event listener to reset button

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     pokemonButton.addEventListener('click', getrandomPokemon);
+    resetButton.addEventListener('click', resetDiv);
 
     
 })
 //NODE Getters
 const pokemonContainer = document.querySelector("#pokemon-container");
 const pokemonButton = document.querySelector("#get-pokemon");
+const resetButton = document.querySelector("#reset");
 
 
 //Helper Functions
@@ -23,10 +25,27 @@ function getrandomPokemon() {
 
 function renderPokemon(pokemon) {
     const pokemonDiv = document.createElement("div");
-    pokemonDiv.innerHTML = `
-        ${pokemon.name}
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png">
-    `
-    pokemonContainer.appendChild(pokemonDiv);
-     
+    const pokeImg = document.createElement("img")
+    const pokeName = document.createElement("h3")
+    const pokeId = document.createElement("p")
+    const pokeType = document.createElement("ul")
+    pokeImg.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+    pokeName.innerText = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    pokeId.innerText = `#${pokemon.id}`
+    pokeType.innerText = pokemonTypes(pokemon.types)
+    pokemonDiv.append(pokeName, pokeImg, pokeId, pokeType)
+    pokemonContainer.appendChild(pokemonDiv);     
+}
+
+function pokemonTypes(typesArray) {
+    if (typesArray.length === 2) {
+        const dualType = (typesArray[0].type.name, typesArray[1].type.name);
+        return dualType
+    } else {
+        return typesArray[0].type.name;
+    }
+}
+
+function resetDiv() {
+    pokemonContainer.innerHTML = " ";
 }

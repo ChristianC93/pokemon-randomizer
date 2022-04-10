710f90745fa9f008047100f88fa7e6e570e70a87
Christian Castillo <castillochristian3@gmail.com>
2022-04-10 16:23:24 -0400

    added styling, html content, js function

--- javascripts/index.js
+++ javascripts/index.js
@@ -10,6 +10,17 @@ const pokemonButton = document.querySelector("#get-pokemon");
 const resetButton = document.querySelector("#reset");
 
 
+//Event Callback Functions
+function getrandomPokemon() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId(1,809)}`)
+    .then(resp => resp.json())
+    .then(pokemon => renderPokemonCard(pokemon))
+}
+
+function resetDiv() {
+    pokemonContainer.innerHTML = " ";
+}
+
 //Helper Functions
 function randomId(min, max) {
     min = Math.ceil(min);
@@ -17,35 +28,48 @@ function randomId(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function getrandomPokemon() {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId(1,809)}`)
-    .then(resp => resp.json())
-    .then(pokemon => renderPokemon(pokemon))
-}
-
 function renderPokemon(pokemon) {
-    const pokemonDiv = document.createElement("div");
-    const pokeImg = document.createElement("img")
-    const pokeName = document.createElement("h3")
-    const pokeId = document.createElement("p")
-    const pokeType = document.createElement("ul")
+    //const pokemonDiv = document.createElement("div");
+    const pokeImg = document.createElement("img");
+    const pokeName = document.createElement("h3");
+    const pokeId = document.createElement("p");
+    const pokeType = document.createElement("ul");
     pokeImg.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
     pokeName.innerText = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-    pokeId.innerText = `#${pokemon.id}`
+    pokeId.innerText = `#${pokemon.id.toString().padStart(3, "0")}`
     pokeType.innerText = pokemonTypes(pokemon.types)
     pokemonDiv.append(pokeName, pokeImg, pokeId, pokeType)
     pokemonContainer.appendChild(pokemonDiv);     
 }
 
 function pokemonTypes(typesArray) {
-    if (typesArray.length === 2) {
-        const dualType = (typesArray[0].type.name, typesArray[1].type.name);
-        return dualType
+    const types = typesArray.map(element => {
+        return element.type.name[0].toUpperCase() + element.type.name.slice(1);
+    });
+    if (types.length === 2) {
+        return types[0] +  "/"  + types[1];
     } else {
-        return typesArray[0].type.name;
-    }
+        return types[0];
+    };  
 }
 
-function resetDiv() {
-    pokemonContainer.innerHTML = " ";
+function renderPokemonCard(pokemon) {
+    const pokemonDiv = document.createElement("div"); 
+    const pokemonCard = ` <article class="card">
+    <header>
+        <h2>${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h2>
+    </header>
+    <div class="image-container">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="${pokemon.name}">
+    </div>
+    <div class="content">
+        <p>${pokemonTypes(pokemon.types)}</p>
+    </div>
+    <footer>#${pokemon.id.toString().padStart(3, "0")}</footer>
+    </article>` 
+    pokemonDiv.innerHTML = pokemonCard;
+    pokemonContainer.appendChild(pokemonDiv);
 }
+
+
+
